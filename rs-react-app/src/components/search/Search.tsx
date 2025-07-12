8fd91d2e19798e67c47c715b077bb94380332ae6
PolinaRomanchuk <polinar_99@mail.ru>
2025-07-12 12:43:40 +0300

    feat: handle search input

--- rs-react-app/src/components/search/Search.tsx
+++ rs-react-app/src/components/search/Search.tsx
@@ -2,11 +2,38 @@ import React from 'react';
 import '../search/search.css';
 
 class Search extends React.Component {
+  state = {
+    searchInput: '',
+  };
+
+  componentDidMount() {
+    const savedSearchInput = localStorage.getItem('searchInput');
+    if (savedSearchInput) {
+      this.setState({ searchInput: savedSearchInput });
+    }
+  }
+
+  handleSearchInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({
+      searchInput: event.target.value,
+    });
+  };
+
+  handleSearch = () => {
+    localStorage.setItem('searchInput', this.state.searchInput);
+  };
+
   render() {
     return (
       <div className="search">
-        <input className="search_input" />
-        <button className="search_button">search</button>
+        <input
+          className="search_input"
+          value={this.state.searchInput}
+          onChange={this.handleSearchInputChange}
+        />
+        <button className="search_button" onClick={this.handleSearch}>
+          search
+        </button>
       </div>
     );
   }
