8722d5e387c5c36d939384ca120dacd7fceacdf4
PolinaRomanchuk <polinar_99@mail.ru>
2025-07-19 17:47:46 +0300

    test: add test to card component

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
-
 export default defineConfig({
   plugins: [react()],
   test: {
