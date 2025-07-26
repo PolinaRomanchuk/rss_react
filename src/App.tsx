4ccb394ede9f6db519f23440990d1113932d9a86
PolinaRomanchuk <polinar_99@mail.ru>
2025-07-26 10:00:08 +0300

    feat: add routing

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,19 @@ import { createRoot } from 'react-dom/client';
 import Main from './components/main/Main.tsx';
 import './global.css';
 import ErrorBoundary from './components/error/ErrorBoundary.tsx';
+import { BrowserRouter, Route, Routes } from 'react-router';
 
 const root = document.getElementById('root');
 if (!root) throw new Error('No root');
 
 createRoot(root).render(
   <StrictMode>
-    <ErrorBoundary>
-      <Main />
-    </ErrorBoundary>
+    <BrowserRouter>
+      <ErrorBoundary>
+        <Routes>
+          <Route path="/" element={<Main />} />
+        </Routes>
+      </ErrorBoundary>
+    </BrowserRouter>
   </StrictMode>
 );
