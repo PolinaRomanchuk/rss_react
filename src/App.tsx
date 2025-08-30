bf65726abe81ddf1797b4a792d0deca809ae4504
PolinaRomanchuk <polinar_99@mail.ru>
2025-08-30 17:10:35 +0300

    feat: add searching

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,48 @@
 import { lazy, Suspense, useEffect, useState } from 'react';
 import './App.css';
 import type { Row } from './types/table-types';
-import { fetchAllCountries } from './services/fetchData';
+import { fetchAllCountries, fetchCountryByName } from './services/fetchData';
 import Spinner from './components/spinner/Spinner';
+import Search from './components/search/Search';
 
 const Table = lazy(() => import('./components/table/Table'));
 
 function App() {
   const [rows, setRows] = useState<Row[]>([]);
   const [currentYear, setCurrentYear] = useState(0);
+  const [searchWord, setSearchWord] = useState('');
+  const [error, setError] = useState('');
 
   useEffect(() => {
-    (async function fetchData() {
-      const { maxYear, rows } = await fetchAllCountries();
-      setCurrentYear(maxYear);
-      setRows(rows);
-    })();
-  }, []);
+    const fetchData = async () => {
+      try {
+        setError('');
+        const { maxYear, rows } = searchWord
+          ? await fetchCountryByName(searchWord)
+          : await fetchAllCountries();
+        setCurrentYear(maxYear);
+        setRows(rows);
+      } catch (error) {
+        if (error instanceof Error) {
+          setError('This country does not find');
+        } else {
+          setError('Something went wrong');
+        }
+      }
+    };
+
+    fetchData();
+  }, [searchWord]);
 
   return (
     <>
+      <Search setSearchWord={setSearchWord} />
       <Suspense fallback={<Spinner />}>
-        <Table rows={rows} currentYear={currentYear} />
+        {!error ? (
+          <Table rows={rows} currentYear={currentYear} />
+        ) : (
+          <div className="error_message">{error}</div>
+        )}
       </Suspense>
     </>
   );
--- src/services/fetchData.ts
+++ src/services/fetchData.ts
@@ -29,3 +29,32 @@ export const fetchAllCountries = async (): Promise<AllCountriesResponse> => {
     throw Error('Something went wrong');
   }
 };
+
+export const fetchCountryByName = async (
+  searchCountry: string
+): Promise<AllCountriesResponse> => {
+  try {
+    const res = await fetch('/owid-co2-data.json');
+    const json: JSONData = await res.json();
+    const countryData = json[searchCountry];
+    if (!countryData) {
+      throw new Error(`This country does not find`);
+    }
+
+    const rows: Row[] = countryData.data.map((row) => ({
+      country: searchCountry,
+      iso_code: countryData.iso_code,
+      population: row.population,
+      year: row.year,
+      cement_co2: row.cement_co2,
+      cement_co2_per_capita: row.cement_co2_per_capita,
+    }));
+
+    const maxYear = Math.max(...rows.map((x) => x.year));
+
+    return { maxYear, rows };
+  } catch (error) {
+    console.log(error);
+    throw Error('Something went wrong');
+  }
+};
