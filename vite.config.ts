defa2f034629820b853f7b7ed81718d8007f762f
PolinaRomanchuk <polinar_99@mail.ru>
2025-10-19 20:39:25 +0300

    style: add tailwind and style header

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@
 import { defineConfig } from 'vitest/config';
 import svgr from 'vite-plugin-svgr';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react(), svgr(), tailwindcss()],
   test: {
     globals: true,
     environment: 'jsdom',
